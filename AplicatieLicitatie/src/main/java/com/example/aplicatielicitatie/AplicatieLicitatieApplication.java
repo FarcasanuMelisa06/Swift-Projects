70a9084a5fdc6cb1fe03492cdad43c9a6e29f051
Melisa Farcasanu <melisa_farcasanu@yahoo.com>
2022-12-09 16:34:48 +0200

    Aplicatie-frontend

---
+++ AplicatieLicitatie/src/main/java/com/example/aplicatielicitatie/AplicatieLicitatieApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplicatielicitatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicatieLicitatieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplicatieLicitatieApplication.class, args);
+    }
+
+}
---
+++ AplicatieLicitatie/src/test/java/com/example/aplicatielicitatie/AplicatieLicitatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplicatielicitatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicatieLicitatieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
